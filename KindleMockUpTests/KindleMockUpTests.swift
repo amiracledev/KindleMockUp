bd4ab061b9eadfef4f652590669264a55d1f5297
Amir Nickroo <amiracledev@gmail.com>
2018-08-04 00:55:23 -0700

    Initial Commit

---
+++ KindleMockUpTests/KindleMockUpTests.swift
@@ -0,0 +1,36 @@
+//
+//  KindleMockUpTests.swift
+//  KindleMockUpTests
+//
+//  Created by Amir Nickroo on 8/4/18.
+//  Copyright © 2018 Amir Nickroo. All rights reserved.
+//
+
+import XCTest
+@testable import KindleMockUp
+
+class KindleMockUpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
